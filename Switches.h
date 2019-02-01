a3a96c3f06972518058c51e1e578829ac9b565fa
Krokoman13 <bramtehaken@gmail.com>
2019-02-01 15:22:03 +0100

    Update Switches.h

--- Switches.h
+++ Switches.h
@@ -1,22 +1,21 @@
 #pragma once
 
-class Switch
+class Switch			
 {
-	//Normal switches
-	int inpin;
-	int position;
+	int inpin;						//=The pin connected to the switch
+	int position;					//=The position of the switch (0 = -150 degrees, 1023 = +150 degrees)
 	
-	bool movable;		//=True when switch is movable
+	bool movable;					//=True when switch is movable by an solenoid
 
 	//Movable switches
-	int outpinUp;
-	int outpinDwn;
+	int outpinUp;					//=The pin connected to H-Bridge
+	int outpinDwn;					//=The other pin connected to H-Bridge
 
 public:
-	Switch(int, int);
-	Switch(int, int, int, int);
-	bool get_switchstate();
-	int get_position();
-	void set_up();
-	void set_down();
+	Switch(int, int);				//Defining a non-movable switch
+	Switch(int, int, int, int);			//Defining a movable switch
+	bool get_switchstate();				//Checking if the switch is switched on
+	int get_position();				//Getting the position of the switch 0-1023 (0 = -150 degrees, 1023 = +150 degrees)
+	void set_up();					//Set the solenoid to UP (if switch is movable)
+	void set_down();				//Set the solenoid to DOWN (if switch is movable)
 };
